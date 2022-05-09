8d815061b62c76b6725c0a3c911e21214d3a55a9
doughbryce <31143323+doughbryce@users.noreply.github.com>
2022-05-09 15:54:23 -0600

    emplyee and artist sql

--- artist.sql
+++ artist.sql
@@ -1,1 +1,19 @@
-select from in is 
+-- INSERT INTO artist(name)
+-- 	VALUES('childish gambino'),
+--   			('bryce doughman'),
+--         ('imagine dragons');
+  
+--  select * from artist
+--  ORDER BY name desc
+--  limit 10;
+
+-- select * from artist
+-- order by name
+-- OFFSET 100
+-- LIMIT 5;
+
+-- select * from artist
+-- where name LIKE 'Black%';
+
+-- select * from artist
+-- where name LIKE '%Black%';
---
+++ employee.sql
@@ -0,0 +1,16 @@
+-- select first_name, last_name FROM employee
+-- where city = 'Calgary';
+
+-- select birth_date from employee
+-- order by birth_date desc
+-- limit 1;
+
+-- select birth_date from employee
+-- order by birth_date
+-- limit 1;
+
+-- select first_name, reports_to from employee
+-- where reports_to = 2;
+
+-- select COUNT(*) from employee
+-- where city = 'Lethbridge';
